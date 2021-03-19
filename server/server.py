74989acf47f5dcfdd0445b3e18a804049092855f
Jainish Shah <jainishnikul2525@gmail.com>
2021-03-20 05:10:00 +0530

    initial commit

---
+++ server/server.py
@@ -0,0 +1,32 @@
+from flask import Flask, request,  jsonify
+import util
+
+app = Flask(__name__)
+#model = pickle.load(open('model.pkl','rb'))
+
+@app.route("/get_location_names")
+def get_location_names():
+    response = jsonify({
+        'locations': util.get_location_names()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route('/predict_house_price',methods=['POST'])
+def predict_house_price():
+    total_sqft = float(request.form['total_sqft'])
+    location = request.form['location']
+    bhk = int(request.form['bhk'])
+    bath = int(request.form['bath'])
+
+    response = jsonify({
+        'estimated_price': util.get_estimated_price(location, total_sqft, bhk, bath)    # hey coder note here that I am passing the parameters for the predictions
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+if __name__ == "__main__":
+    print("Starting Python Flask Server For Home Price Predictions....")
+    util.load_saved_artifacts()
+    app.run()
